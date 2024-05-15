e81fa727d43c4470cf4255fe28f4d8509c41564f
Alex Noah <67859130+AmirrezaM-dev@users.noreply.github.com>
2024-05-15 18:14:47 +0400

    Update PreLoader.js

--- src/Pages/PreLoader.js
+++ src/Pages/PreLoader.js
@@ -19,7 +19,7 @@ const Preloader = () => {
 					expires: new Date(Date.now() + 900000),
 				})
 			}
-		}, 1666) // Update progress every second
+		}, 1051) // Update progress every second
 
 		return () => clearInterval(interval)
 		// eslint-disable-next-line react-hooks/exhaustive-deps
